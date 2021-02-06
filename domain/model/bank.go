de1cb49d29a7398469e5e9634eb5b2c0139d0e0a
Raphael <raphael@agecomnet.com.br>
2021-02-06 16:35:20 -0300

    Aula 02 part 4

--- domain/model/bank.go
+++ domain/model/bank.go
@@ -1,37 +1,41 @@
 package model
 
-import {
+import (
 	"time"
-	uuid "github.com/satori/go-uuid"
+
+	"github.com/asaskevich/govalidator"
+	uuid "github.com/satori/go.uuid"
+)
+
+func init() {
+	govalidator.SetFieldsRequiredByDefault(true)
 }
 
 type Bank struct {
-	Base `valid:"required"`
-	Code string `json:"code" valid:"notnull"`
-	Name string  `json:"name" valid:"notnull"`
+	Base     `valid:"required"`
+	Code     string     `json:"code" gorm:"type:varchar(20)" valid:"notnull"`
+	Name     string     `json:"name" gorm:"type:varchar(255)" valid:"notnull"`
 	Accounts []*Account `gorm:"ForeignKey:BankID" valid:"-"`
 }
 
-func (bank *Bank) isValid error{
-	_,err := govalidator.ValidateStruct(bank)
-	if err != nil{
+func (bank *Bank) isValid() error {
+	_, err := govalidator.ValidateStruct(bank)
+	if err != nil {
 		return err
 	}
 	return nil
 }
 
-func NewBank(Code string name string) (*Bank, error) {
+func NewBank(code string, name string) (*Bank, error) {
 	bank := Bank{
-		Code:code,
-		Name:name,
+		Code: code,
+		Name: name,
 	}
-	bank.ID = uuid.NewUUID().String()
-	bank.created_at =time.Now()
-
+	bank.ID = uuid.NewV4().String()
+	bank.CreatedAt = time.Now()
 	err := bank.isValid()
 	if err != nil {
-		return nil,err
+		return nil, err
 	}
-
-	return &bank,nil
+	return &bank, nil
 }
